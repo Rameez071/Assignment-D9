5fc371f5394de68f72033804fd527f2a80975cf2
Rameez071 <rameezraja405@gmail.com>
2020-08-16 23:07:16 +0530

    employee wage case script added

---
+++ EmpWageCase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+echo "Enter 1 for Parttime, 2 for Fulltime && 0 for NotPresent"
+"Enter your Emp Type:"
+read -p forTypeOfEmployee
+empRatePerHr=20;
+empCheck=$forTypeOfEmployee;
+case $empCheck in
+        $isFullTime)
+                empHrs=8
+                ;;
+        $isPartTime)
+                empHrs=4
+                ;;
+        *)
+        empHrs=0
+                ;;
+esac
+
+salary=$(($empHrs*$empRatePerHr));
